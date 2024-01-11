b1b74e5a6270fd330917a058620326570b782a46
Rahul Varma Mandapati <rahulvarmamandapati@Rahul-Varma.local>
2024-01-11 10:30:39 +0530

    Updates

---
+++ precache-manifest.c02e11f7def7fbd950c4317f98edf99f.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afde728e91ee4fb43a1c31adea1aa62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47cce2c865d9f388f12c",
+    "url": "/static/css/7.c98225b9.chunk.css"
+  },
+  {
+    "revision": "ddb618dd9259a36cbc98",
+    "url": "/static/css/main.58662316.chunk.css"
+  },
+  {
+    "revision": "9e760ea34467f69e05f0",
+    "url": "/static/js/0.e55abbb1.chunk.js"
+  },
+  {
+    "revision": "406a96ee7f7b4938ee35",
+    "url": "/static/js/1.fb386119.chunk.js"
+  },
+  {
+    "revision": "0cfe320bdcdbf3404d61",
+    "url": "/static/js/10.32c05540.chunk.js"
+  },
+  {
+    "revision": "b80d9498f500a70831d2",
+    "url": "/static/js/11.a8b2baa1.chunk.js"
+  },
+  {
+    "revision": "54bd4457e3d8d630514b",
+    "url": "/static/js/12.7fe24df0.chunk.js"
+  },
+  {
+    "revision": "65fc96003a92f8395758",
+    "url": "/static/js/13.2e5167a2.chunk.js"
+  },
+  {
+    "revision": "bc39b9c2b3e29f425834",
+    "url": "/static/js/14.b11091d1.chunk.js"
+  },
+  {
+    "revision": "faa7045d94d81b59d07a",
+    "url": "/static/js/15.30b88c7d.chunk.js"
+  },
+  {
+    "revision": "ec59fb8d907a267df56a",
+    "url": "/static/js/2.de836e49.chunk.js"
+  },
+  {
+    "revision": "bef6c4b73edd9dbf64ed",
+    "url": "/static/js/3.d88bfe4d.chunk.js"
+  },
+  {
+    "revision": "d4b7f00200c9cc2b636f",
+    "url": "/static/js/4.05b7ae45.chunk.js"
+  },
+  {
+    "revision": "47cce2c865d9f388f12c",
+    "url": "/static/js/7.61144c94.chunk.js"
+  },
+  {
+    "revision": "0a877056871850891ad6",
+    "url": "/static/js/8.1edff998.chunk.js"
+  },
+  {
+    "revision": "1b04aa08418c71d6f474",
+    "url": "/static/js/9.98c80497.chunk.js"
+  },
+  {
+    "revision": "ddb618dd9259a36cbc98",
+    "url": "/static/js/main.266fa559.chunk.js"
+  },
+  {
+    "revision": "4b484437c27c8bdbc02d",
+    "url": "/static/js/runtime~main.757a98d1.js"
+  },
+  {
+    "revision": "811874136a8ae5658743307479f6b867",
+    "url": "/static/media/AvatarSprite.81187413.svg"
+  },
+  {
+    "revision": "d14ad1035ae6da4e5a71eca362a8d696",
+    "url": "/static/media/Montserrat-Bold.d14ad103.ttf"
+  },
+  {
+    "revision": "34de1239b12123b85ff1a68b58835a1f",
+    "url": "/static/media/Montserrat-Regular.34de1239.ttf"
+  },
+  {
+    "revision": "5d9c9c73f9871e0b4c71c1ae47e68cb3",
+    "url": "/static/media/avatar03.5d9c9c73.png"
+  },
+  {
+    "revision": "6fa51bdc5a44ff9c8a779f4c74956c77",
+    "url": "/static/media/avatar04.6fa51bdc.png"
+  },
+  {
+    "revision": "223101aa39fe1570e17431d4bb025b72",
+    "url": "/static/media/avatar05.223101aa.png"
+  },
+  {
+    "revision": "cf2c45b9763a825d1fb2a610ed1b9afb",
+    "url": "/static/media/avatar08.cf2c45b9.png"
+  },
+  {
+    "revision": "2bff1bd75f3f2be280106810f7d43795",
+    "url": "/static/media/avatar09.2bff1bd7.png"
+  },
+  {
+    "revision": "12a372a3d3927993864bdbc5738a0841",
+    "url": "/static/media/avatar11.12a372a3.png"
+  },
+  {
+    "revision": "2fa4bb5a9fe76fdcbefce4145d8c3ad1",
+    "url": "/static/media/avatar12.2fa4bb5a.png"
+  },
+  {
+    "revision": "4fb4d1176a2a398ca43922f8a8c66621",
+    "url": "/static/media/bg-blue-cylinder-pattern.4fb4d117.jpg"
+  },
+  {
+    "revision": "dd386729f83fdb2f1f0a29b544c4b1b9",
+    "url": "/static/media/bg-blue-dots-pattern.dd386729.jpg"
+  },
+  {
+    "revision": "fa757370c2dc12c3e797fb72e6daafbf",
+    "url": "/static/media/bg-blue-spheres-pattern.fa757370.jpg"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "a45f236ee119f5ef0db8395e8d81dc3b",
+    "url": "/static/media/hero-imag-bg-001.a45f236e.png"
+  },
+  {
+    "revision": "ef91403e3f2798566ce3127534ddb262",
+    "url": "/static/media/hero-imag-bg-002.ef91403e.png"
+  },
+  {
+    "revision": "e30bc9ebfdb87a7f3fe6bfe2e9de619a",
+    "url": "/static/media/icon-arrow-left-sm.e30bc9eb.svg"
+  },
+  {
+    "revision": "36e559f8a6801c6e3c39196af20e14d3",
+    "url": "/static/media/icon-arrow-left.36e559f8.svg"
+  },
+  {
+    "revision": "3d10e91b1a7248944c6b24d042bb1335",
+    "url": "/static/media/icon-arrow-right.3d10e91b.svg"
+  },
+  {
+    "revision": "108f33ad155fea8ed57b944e8969061f",
+    "url": "/static/media/icon-arrow-thin-red.108f33ad.svg"
+  },
+  {
+    "revision": "7a5be747ac888397b5b006fe369a2bcb",
+    "url": "/static/media/icon-button-arrow-right-red.7a5be747.svg"
+  },
+  {
+    "revision": "c097cbf65effbd44ddd55c5454e32bea",
+    "url": "/static/media/icon-button-arrow-right.c097cbf6.svg"
+  },
+  {
+    "revision": "9d9265e6cc663ed9051bd0915de0991b",
+    "url": "/static/media/icon-checkmark-circle-orange.9d9265e6.svg"
+  },
+  {
+    "revision": "0567da99fbd13589c55663e7aadab84f",
+    "url": "/static/media/icon-close.0567da99.svg"
+  },
+  {
+    "revision": "69e2d73869528b4dbb9b47aaf47d31f1",
+    "url": "/static/media/icon-comment.69e2d738.svg"
+  },
+  {
+    "revision": "1a7fad6ac1edb7fd1d27fb7891289308",
+    "url": "/static/media/icon-knowledge.1a7fad6a.svg"
+  },
+  {
+    "revision": "0ee100d1321e6b9ad3caea66dd92bbda",
+    "url": "/static/media/icon-skills.0ee100d1.svg"
+  },
+  {
+    "revision": "7a29b3a8002f9c7fb8dffff43dfc812a",
+    "url": "/static/media/icon-snap.7a29b3a8.svg"
+  },
+  {
+    "revision": "64b00ce0f43bebf63956e7235c912c6c",
+    "url": "/static/media/icon-tool-tip.64b00ce0.svg"
+  },
+  {
+    "revision": "d195aac7bd3106dbd36b007368713fc9",
+    "url": "/static/media/icons-behaviors.d195aac7.svg"
+  },
+  {
+    "revision": "dfb3aec8b4151f17335f3253833b285d",
+    "url": "/static/media/mindtickle-logo-orange.dfb3aec8.svg"
+  },
+  {
+    "revision": "a578a6fb9493f485550017789fdff597",
+    "url": "/static/media/mindtickle-logo-white.a578a6fb.svg"
+  },
+  {
+    "revision": "2723c08a2541aa0fa782ea80330aeaaa",
+    "url": "/static/media/pdf-third-page-ad.2723c08a.jpg"
+  },
+  {
+    "revision": "5759d048e45aff48ef30b1db3887dc0d",
+    "url": "/static/media/score-card-thumbnail-001@2x.5759d048.png"
+  },
+  {
+    "revision": "19d874792fc47063e6e84e1d805496e0",
+    "url": "/static/media/the-buyer-001@2x.19d87479.png"
+  },
+  {
+    "revision": "1c890573b35350142931c26e82d3af23",
+    "url": "/static/media/the-seller-001@2x.1c890573.png"
+  }
+]);
